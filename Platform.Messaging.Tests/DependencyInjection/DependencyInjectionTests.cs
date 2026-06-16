d2ace6a4bf60d9a1802d9c5f789ee64efa4fbc29
Khánh Hưng <khanhhungzm@gmail.com>
2026-06-16 09:44:33 +0700

    fix(messaging): import Platform.Messaging.Abstractions namespace in dependency injection tests

--- Platform.Messaging.Tests/DependencyInjection/DependencyInjectionTests.cs
+++ Platform.Messaging.Tests/DependencyInjection/DependencyInjectionTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Platform.Messaging.Abstractions;
 using Platform.Messaging.Configurations;
 using Platform.Messaging.Constants;
 using Platform.Messaging.DependencyInjection;
